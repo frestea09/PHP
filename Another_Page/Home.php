d03547ba8c3bb8c43e07d88ebd15472e14cd9c89
ilman teguh prasetya <ilmanfrasetya@gmail.com>
2019-02-15 18:33:30 +0700

    i create for link to antoher page

---
+++ Another_Page/Home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="in">
+<head>
+    <title>
+        Hello World
+    </title>
+</head>
+<body>
+<?php
+echo "hello World";
+echo "Hello I create something good";
+echo "Saya Ganteng";
+?>
+</body>
+</html>
