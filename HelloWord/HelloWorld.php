71b7d629e4a93af0978f4b29e17fda5df80579ec
ilman teguh prasetya <ilmanfrasetya@gmail.com>
2019-02-15 18:03:07 +0700

    Hellow World In PHP
    HelloWorld

---
+++ HelloWord/HelloWorld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="in">
+    <head>
+        <title>
+            Hello World
+        </title>
+    </head>
+    <body>
+        <?php
+            echo "hello World";
+        ?>
+    </body>
+</html>
